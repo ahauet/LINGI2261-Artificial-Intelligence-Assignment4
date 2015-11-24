1264037f7d82caa9c205b40d30ab7967bcf6c852
Kagelmacher <alexandre.hauet@gmail.com>
2015-11-24 18:19:12 +0100

    final greedy

--- wedding_problem/template.py
+++ wedding_problem/template.py
@@ -19,7 +19,7 @@ class Wedding(Problem):
         self.a = []
         self.read_input_file(input_file)
         size_table = int(self.n / self.t)
-        self.tables = [[None] * size_table] * self.t
+        self.tables = [[None for x in range(0,size_table)] for y in range(0,self.t)]
         self.value = 0
         self.initial = State(self.n, self.t, self.a, self.tables, self.value)
 
@@ -85,39 +85,36 @@ def maxvalue(problem, limit=100, callback=None):
     pass
 
 def greedy(problem):
-    s = int(problem.n / problem.t)
-    assigned_person = [0] * problem.n
-    table_index = 0
-    for i in range(0, len(assigned_person)):
-        if assigned_person[i] == 0:
-            friends = ([(x, problem.a[i][x]) for x in range(0, len(problem.a[i]))])
-            friends.pop(i)
-            friends.sort(key = greedy_comp, reverse=True)
-            print('friends=', friends)
+     s = int(problem.n / problem.t)
+     assigned_person = [0] * problem.n
+     for table in problem.tables:
+         seat = 0
+         alone_people = find_allow_person(assigned_person)
+         if alone_people == -1:
+             print("Error : everyone is at the table ")
+         table[seat] = alone_people
+         seat += 1
+         assigned_person[alone_people] = 1
+         available_friends = get_potential_new_friends(alone_people, assigned_person, problem.a)
+         available_friends.sort(key = greedy_comp, reverse=True)
+         for x in range(0,s-1):
+             friend = available_friends[x]
+             table[seat]  = friend[0]
+             seat += 1
+             assigned_person[friend[0]] = 1
 
-            #add itself to the table
-            problem.tables[table_index][0] = i
-            assigned_person[i] = 1
+def find_allow_person(list):
+    for x in range(0, len(list)):
+        if list[x] == 0:
+            return x
+    return -1
 
-            temp_s = s
-            k = 0
-            next_index_to_add = 1
-            while k < temp_s:
-                print('k',k)
-                if assigned_person[friends[k][0]] == 0: # don't be friend with an already assigned friend
-                    # if k == s-1:
-                    #     problem.tables[table_index][next_index_to_add] = i
-                    #     assigned_person[i] = 1
-                    #     next_index_to_add += 1
-                    # else:
-                    problem.tables[table_index][next_index_to_add] = friends[k][0]
-                    assigned_person[friends[k][0]] = 1
-                    next_index_to_add += 1
-                else:
-                    temp_s += 1
-                k += 1
-            table_index += 1
-
+def get_potential_new_friends(seeker, assigned_person, friendship_table):
+    result=[]
+    for x in range(seeker+1, len(assigned_person)):
+        if assigned_person[x] == 0:
+            result.append((x,friendship_table[seeker][x]))
+    return result
 
 def greedy_comp(tuple):
     return tuple[1]
